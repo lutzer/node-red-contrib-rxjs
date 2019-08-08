b786c525d34e8013aac5868dbd326835694e8472
Lutz Reiter <lutz.reiter@vw-fc.com>
2019-08-08 16:22:31 +0200

    added filter to operator node

---
+++ src/rx-operator.js
@@ -0,0 +1,78 @@
+const { take, takeUntil, tap } = require('rxjs/operators');
+const { NodeRedObservable } = require('./common.js');
+
+module.exports = function (RED) {
+	function RxNode (config) {
+		RED.nodes.createNode(this, config);
+
+        var node = this;
+        var globalContext = node.context().global;
+
+        function showState(state) {
+            switch (state) {
+                case "no-pipe":
+                    node.status({ fill: "red", shape: "ring", text: "missing pipe"});
+                    break;
+                case "piped":
+                    node.status({ fill: "green", shape: "dot", text: "piped"});
+                    break;
+                case "no-argument":
+                    node.status({ fill: "yellow", shape: "ring", text: "missing argument"});
+                case "no-operator":
+                    node.status({ fill: "red", shape: "ring", text: "missing operator"});
+                    break;
+            }
+        }
+
+        if (config.operatorType === "none") 
+            showState('no-operator');
+        else
+            showState("no-pipe");
+
+        var observableWrapper = new NodeRedObservable(node);
+
+        node.on('input', function (msg) {
+            switch (config.operatorType) {
+                case "take":
+                    if (msg.topic === 'pipe') {
+                        var $observable = globalContext.get(msg.payload.observable)
+                        observableWrapper.register(
+                            $observable.pipe(
+                                take(config.take_count),
+                                tap( (msg) => node.send([null, msg]))
+                            )
+                        )
+                        node.send([observableWrapper.pipeMessage, null]);
+                        showState("piped");
+                    }
+                    break;
+                case "takeUntil":
+                    if (msg.topic === 'pipe') {
+                        node.$pipeObservable = globalContext.get(msg.payload.observable)
+                        showState("no-argument");
+                    } else if (msg.topic === 'until') {
+                        node.$untilObservable = globalContext.get(msg.payload.observable)
+                        showState("no-pipe");
+                    }
+                    if (node.$pipeObservable && node.$untilObservable) {
+                        showState("piped");
+                        observableWrapper.register(
+                            node.$pipeObservable.pipe(
+                                takeUntil(node.$untilObservable),
+                                tap( (msg) => node.send([null, msg]))
+                            )
+                        )
+                        node.send([observableWrapper.pipeMessage, null]);
+                    }
+                    break;
+                default:
+                    showState("no-operator")
+            }	
+        });
+
+		node.on('close', function () {
+            observableWrapper.remove();
+		});
+	}
+	RED.nodes.registerType("rx operator", RxNode);
+};
